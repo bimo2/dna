c8b657e0282aa973747f7234c8667514f5355b90
Bimal Bhagrath <48932917+bimo2@users.noreply.github.com>
2021-08-10 23:26:51 -0400

    Setup Xcode objective-c project

---
+++ alo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  alo
+//
+//  Created by Bimal Bhagrath on 2021-08-10.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    
+    return 0;
+}
